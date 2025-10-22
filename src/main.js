9e39e69dc7b46fd61830f1625da477458551d87a
Shinichiro Kuwahara <kenbunroku524@gmail.com>
2025-10-23 08:09:36 +0900

    Implement firework particle system in Cesium; remove Three.js integration and related code.

--- src/main.js
+++ src/main.js
@@ -1,7 +1,5 @@
 import * as Cesium from "cesium";
 import "cesium/Build/Cesium/Widgets/widgets.css";
-import * as THREE from "three";
-import { createColoredCube } from "./cube";
 
 Cesium.Ion.defaultAccessToken =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJkYTE4NjhhMy01YjM0LTQ0MDYtOThjMi1hNWJlYmI5MWY3YzQiLCJpZCI6MzQ2NzMzLCJpYXQiOjE3NTk0NTA4NDN9.IQ-97lJJ-qTrkTUllzEMiMAIgCVSEb9eQxwIbSJ_zjo";
@@ -18,6 +16,138 @@ const position = Cesium.Cartesian3.fromDegrees(
   location.height
 );
 
+const modelMatrix = Cesium.Transforms.eastNorthUpToFixedFrame(
+  Cesium.Cartesian3.fromDegrees(location.lng, location.lat)
+);
+
+const emitterInitialLocation = new Cesium.Cartesian3(
+  location.lng,
+  location.lat,
+  location.height
+);
+
+let particleCanvas;
+
+function getImage() {
+  if (!Cesium.defined(particleCanvas)) {
+    particleCanvas = document.createElement("canvas");
+    particleCanvas.width = 20;
+    particleCanvas.height = 20;
+    const context2D = particleCanvas.getContext("2d");
+    context2D.beginPath();
+    context2D.arc(8, 8, 8, 0, Cesium.Math.TWO_PI, true);
+    context2D.closePath();
+    context2D.fillStyle = "rgb(255, 255, 255)";
+    context2D.fill();
+  }
+  return particleCanvas;
+}
+
+const minimumExplosionSize = 30.0;
+const maximumExplosionSize = 100.0;
+const particlePixelSize = new Cesium.Cartesian2(7.0, 7.0);
+const burstSize = 400.0;
+const lifetime = 10.0;
+const numberOfFireworks = 20.0;
+
+const emitterModelMatrixScratch = new Cesium.Matrix4();
+
+function createFirework(offset, color, bursts) {
+  const position = Cesium.Cartesian3.add(
+    emitterInitialLocation,
+    offset,
+    new Cesium.Cartesian3()
+  );
+  const emitterModelMatrix = Cesium.Matrix4.fromTranslation(
+    position,
+    emitterModelMatrixScratch
+  );
+  const particleToWorld = Cesium.Matrix4.multiply(
+    modelMatrix,
+    emitterModelMatrix,
+    new Cesium.Matrix4()
+  );
+  const worldToParticle = Cesium.Matrix4.inverseTransformation(
+    particleToWorld,
+    particleToWorld
+  );
+
+  const size = Cesium.Math.randomBetween(
+    minimumExplosionSize,
+    maximumExplosionSize
+  );
+  const particlePositionScratch = new Cesium.Cartesian3();
+  const force = function (particle) {
+    const position = Cesium.Matrix4.multiplyByPoint(
+      worldToParticle,
+      particle.position,
+      particlePositionScratch
+    );
+    if (Cesium.Cartesian3.magnitudeSquared(position) >= size * size) {
+      Cesium.Cartesian3.clone(Cesium.Cartesian3.ZERO, particle.velocity);
+    }
+  };
+
+  const normalSize =
+    (size - minimumExplosionSize) /
+    (maximumExplosionSize - minimumExplosionSize);
+  const minLife = 0.3;
+  const maxLife = 1.0;
+  const life = normalSize * (maxLife - minLife) + minLife;
+
+  scene.primitives.add(
+    new Cesium.ParticleSystem({
+      image: getImage(),
+      startColor: color,
+      endColor: color.withAlpha(0.0),
+      particleLife: life,
+      speed: 100.0,
+      imageSize: particlePixelSize,
+      emissionRate: 0,
+      emitter: new Cesium.SphereEmitter(0.1),
+      bursts: bursts,
+      lifetime: lifetime,
+      updateCallback: force,
+      modelMatrix: modelMatrix,
+      emitterModelMatrix: emitterModelMatrix,
+    })
+  );
+}
+
+const xMin = -100.0;
+const xMax = 100.0;
+const yMin = -80.0;
+const yMax = 100.0;
+const zMin = -50.0;
+const zMax = 50.0;
+
+const colorOptions = [
+  {
+    minimumRed: 0.75,
+    green: 0.0,
+    minimumBlue: 0.8,
+    alpha: 1.0,
+  },
+  {
+    red: 0.0,
+    minimumGreen: 0.75,
+    minimumBlue: 0.8,
+    alpha: 1.0,
+  },
+  {
+    red: 0.0,
+    green: 0.0,
+    minimumBlue: 0.8,
+    alpha: 1.0,
+  },
+  {
+    minimumRed: 0.75,
+    minimumGreen: 0.75,
+    blue: 0.0,
+    alpha: 1.0,
+  },
+];
+
 const viewer = new Cesium.Viewer("cesiumContainer", {
   terrainProvider: await Cesium.CesiumTerrainProvider.fromIonAssetId(2767062),
   baseLayerPicker: false,
@@ -25,9 +155,11 @@ const viewer = new Cesium.Viewer("cesiumContainer", {
   animation: false,
   timeline: false,
   shadows: true,
+  shouldAnimate: true,
 });
 const scene = viewer.scene;
 scene.globe.depthTestAgainstTerrain = true;
+scene.debugShowFramesPerSecond = true;
 
 try {
   // Load the Japan Building data asset
@@ -57,70 +189,6 @@ scene.preRender.addEventListener(function (scene, time) {
   );
 });
 
-// Three.js
-const threeContainer = document.getElementById("three");
-const threeScene = new THREE.Scene();
-const threeCamera = new THREE.PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight,
-  1,
-  50000000
-);
-const threeRenderer = new THREE.WebGLRenderer({ alpha: true });
-threeRenderer.setSize(window.innerWidth, window.innerHeight);
-threeContainer?.appendChild(threeRenderer.domElement);
-
-// Create a cube in Three.js and add it to the scene
-const cube = createColoredCube();
-cube.position.set(position.x, position.y, position.z);
-threeScene.add(cube);
-
-// Direct the Cesium camera to look at the cube
-viewer.camera.lookAt(position, new Cesium.Cartesian3(100.0, 500.0, 300.0));
-viewer.camera.lookAtTransform(Cesium.Matrix4.IDENTITY);
-
-function updateThreeJS() {
-  // Update Three.js camera field of view to match Cesium camera's vertical FOV
-  threeCamera.fov = Cesium.Math.toDegrees(viewer.camera.frustum.fovy);
-  threeCamera.updateProjectionMatrix();
-
-  // Sync Three.js camera with Cesium camera
-  const cesiumCamera = viewer.camera;
-  const cvm = cesiumCamera.viewMatrix;
-  const civm = cesiumCamera.inverseViewMatrix;
-
-  // Fix the extraction of camera position and direction from matrices
-  const cameraPosition = Cesium.Cartesian3.fromElements(
-    civm[12],
-    civm[13],
-    civm[14]
-  );
-  const cameraDirection = new Cesium.Cartesian3(-cvm[2], -cvm[6], -cvm[10]);
-  const cameraUp = new Cesium.Cartesian3(cvm[1], cvm[5], cvm[9]);
-
-  const cameraPositionVec3 = new THREE.Vector3(
-    cameraPosition.x,
-    cameraPosition.y,
-    cameraPosition.z
-  );
-  const cameraDirectionVec3 = new THREE.Vector3(
-    cameraDirection.x,
-    cameraDirection.y,
-    cameraDirection.z
-  );
-  const cameraUpVec3 = new THREE.Vector3(cameraUp.x, cameraUp.y, cameraUp.z);
-
-  // Update Three.js camera position and orientation
-  threeCamera.position.copy(cameraPositionVec3);
-  threeCamera.up.copy(cameraUpVec3);
-  threeCamera.lookAt(cameraPositionVec3.clone().add(cameraDirectionVec3));
-
-  // Apply rotation to the cube
-  cube.rotation.x += 0.01;
-  // Render the scene with the updated camera
-  threeRenderer.render(threeScene, threeCamera);
-}
-
 window.addEventListener("resize", () => {
   const width = window.innerWidth;
   const height = window.innerHeight;
@@ -130,22 +198,51 @@ window.addEventListener("resize", () => {
   threeCamera.updateProjectionMatrix();
 });
 
-function renderLoop() {
-  requestAnimationFrame(renderLoop);
-  viewer.render();
-  updateThreeJS();
+for (let i = 0; i < numberOfFireworks; ++i) {
+  const x = Cesium.Math.randomBetween(xMin, xMax);
+  const y = Cesium.Math.randomBetween(yMin, yMax);
+  const z = Cesium.Math.randomBetween(zMin, zMax);
+  const offset = new Cesium.Cartesian3(x, y, z);
+  const color = Cesium.Color.fromRandom(colorOptions[i % colorOptions.length]);
+
+  const bursts = [];
+  for (let j = 0; j < 3; ++j) {
+    bursts.push(
+      new Cesium.ParticleBurst({
+        time: Cesium.Math.nextRandomNumber() * lifetime,
+        minimum: burstSize,
+        maximum: burstSize,
+      })
+    );
+  }
+
+  createFirework(offset, color, bursts);
 }
 
-renderLoop();
+const camera = viewer.scene.camera;
+const cameraOffset = new Cesium.Cartesian3(-300.0, 0.0, 0.0);
+camera.lookAtTransform(modelMatrix, cameraOffset);
+camera.lookAtTransform(Cesium.Matrix4.IDENTITY);
 
-// scene.camera.flyTo({
-//   destination: Cesium.Cartesian3.fromDegrees(
-//     location.lng,
-//     location.lat,
-//     location.height
-//   ),
-//   orientation: {
-//     heading: Cesium.Math.toRadians(200),
-//     pitch: Cesium.Math.toRadians(-7),
-//   },
-// });
+const toFireworks = Cesium.Cartesian3.subtract(
+  emitterInitialLocation,
+  cameraOffset,
+  new Cesium.Cartesian3()
+);
+Cesium.Cartesian3.normalize(toFireworks, toFireworks);
+const angle =
+  Cesium.Math.PI_OVER_TWO -
+  Math.acos(Cesium.Cartesian3.dot(toFireworks, Cesium.Cartesian3.UNIT_Z));
+camera.lookUp(angle);
+
+scene.camera.flyTo({
+  destination: Cesium.Cartesian3.fromDegrees(
+    location.lng,
+    location.lat,
+    location.height
+  ),
+  orientation: {
+    heading: Cesium.Math.toRadians(200),
+    pitch: Cesium.Math.toRadians(-7),
+  },
+});
