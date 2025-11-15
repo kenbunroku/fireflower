58336bc0001b6ffe34d18098f7eb4253cde0f19d
Shinichiro Kuwahara <kenbunroku524@gmail.com>
2025-11-15 09:51:46 +0900

    Refactor fragment and vertex shaders to implement bloom effect and enhance point sprite rendering

--- src/shader/fragment.glsl
+++ src/shader/fragment.glsl
@@ -1,7 +1,14 @@
-varying vec2 vUv;
+uniform vec4 u_color;
+uniform float u_time;
+uniform float u_bloomDuration;
 
 void main() {
-  vec2 uv = vUv;
-  vec3 color = vec3(uv.x, uv.y, 0.0);
-  gl_FragColor = vec4(color, 1.0);
+      // 円形スプライトにしたい場合は距離でマスク
+  vec2 uv = gl_PointCoord - 0.5;
+  if(length(uv) > 0.5)
+    discard;
+
+  float alpha = 1. - smoothstep(0.0, 1.0, u_time / u_bloomDuration);
+
+  out_FragColor = vec4(u_color.xyz, u_color.a * alpha);
 }
--- src/shader/vertex.glsl
+++ src/shader/vertex.glsl
@@ -1,6 +1,38 @@
-varying vec2 vUv;
+in vec3 position3DHigh;
+in vec3 position3DLow;
+in float batchId;
+in float delay;
+in vec3 unitDir;
+
+    // 点サイズ
+uniform float u_pointSize;
+uniform float u_time;
+uniform float u_amplitude;
+uniform float u_period;
+uniform float u_radius;
+uniform float u_launchHeight;
+uniform float u_launchProgress;
+uniform float u_bloomDuration;
+
+float easingOutQuint(float t) {
+  return 1.0 - pow(1.0 - t, 5.0);
+}
 
 void main() {
-  vUv = uv;
-  gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);
+  vec4 p = czm_translateRelativeToEye(position3DHigh, position3DLow);
+  vec3 dir = unitDir;
+
+  float t = max(u_time - delay, 0.0);
+
+  float d1 = easingOutQuint(t / u_bloomDuration);
+  float grav = 9.8;
+  vec3 gravP = vec3(0., 0., -1.) * (t * t * grav) * 0.5;
+  float launchProgress = clamp(u_launchProgress, 0.0, 1.0);
+  float launchOffset = u_launchHeight * launchProgress;
+  vec3 newP = (p.xyz + dir * d1 * u_radius) + gravP;
+  newP.z += launchOffset;
+  p.xyz = newP;
+
+  gl_Position = czm_modelViewProjectionRelativeToEye * vec4(p.xyz, 1.);
+  gl_PointSize = u_pointSize * (1. - delay * 1.);
 }
