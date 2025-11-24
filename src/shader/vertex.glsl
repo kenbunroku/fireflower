cf9a4f8eaed5a28b2bc67703a82dced888d97fca
Shinichiro Kuwahara <kenbunroku524@gmail.com>
2025-11-24 23:22:01 +0900

    Refactor vertex shader to normalize direction vector and improve displacement calculation

--- src/shader/vertex.glsl
+++ src/shader/vertex.glsl
@@ -3,6 +3,7 @@ in vec3 position3DLow;
 in float batchId;
 in float delay;
 in vec3 unitDir;
+in float baseRadius;
 
 uniform float u_pointSize;
 uniform float u_time;
@@ -18,16 +19,21 @@ float easingOutQuint(float t) {
 
 void main() {
   vec4 p = czm_translateRelativeToEye(position3DHigh, position3DLow);
-  vec3 dir = unitDir;
+  vec3 dir = normalize(unitDir);
 
   float t = max(u_time - delay, 0.0);
-
   float d1 = easingOutQuint(t / u_bloomDuration);
+
+  float scaleFactor = d1 * u_radius; // 時間経過で 0 -> u_radius まで増える
+
+  // 移動量 = 方向 * 元の距離 * 拡大したい倍率
+  vec3 displacement = unitDir * baseRadius * scaleFactor;
+
   float grav = 9.8;
   vec3 gravP = vec3(0., 0., -1.) * (t * t * grav) * 0.5 * u_gravityStrength;
   float launchProgress = clamp(u_launchProgress, 0.0, 1.0);
   float launchOffset = u_launchHeight * launchProgress;
-  vec3 newP = (p.xyz + dir * d1 * u_radius) + gravP;
+  vec3 newP = (p.xyz + displacement) + gravP;
   newP.z += launchOffset;
   p.xyz = newP;
 
