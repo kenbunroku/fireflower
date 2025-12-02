d508df95f5da0e521e5e1e0339ad22c309036be3
Shinichiro Kuwahara <kenbunroku524@gmail.com>
2025-12-02 14:52:36 +0900

    feat: Add rotation functions for positions around Y and Z axes

--- src/util.js
+++ src/util.js
@@ -25,3 +25,37 @@ export const rotatePositionsAroundX = (positions, angleRad) => {
 
   return rotated;
 };
+
+export const rotatePositionsAroundY = (positions, angleRad) => {
+  const cos = Math.cos(angleRad);
+  const sin = Math.sin(angleRad);
+  const rotated = new Float32Array(positions.length);
+
+  for (let i = 0; i < positions.length; i += 3) {
+    const x = positions[i];
+    const y = positions[i + 1];
+    const z = positions[i + 2];
+    rotated[i] = x * cos + z * sin;
+    rotated[i + 1] = y;
+    rotated[i + 2] = -x * sin + z * cos;
+  }
+
+  return rotated;
+};
+
+export const rotatePositionsAroundZ = (positions, angleRad) => {
+  const cos = Math.cos(angleRad);
+  const sin = Math.sin(angleRad);
+  const rotated = new Float32Array(positions.length);
+
+  for (let i = 0; i < positions.length; i += 3) {
+    const x = positions[i];
+    const y = positions[i + 1];
+    const z = positions[i + 2];
+    rotated[i] = x * cos - y * sin;
+    rotated[i + 1] = x * sin + y * cos;
+    rotated[i + 2] = z;
+  }
+
+  return rotated;
+};
